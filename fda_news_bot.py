f3164606f8c9e72e86b3e1e1b7617fa9e79fb473
IBROXIMJON0411 <abdulloh0411i@gmail.com>
2025-12-29 23:32:25 +0300

    Update fda_news_bot.py

--- fda_news_bot.py
+++ fda_news_bot.py
@@ -74,7 +74,7 @@ signal.signal(signal.SIGTERM, _handle_sig)
 
 # --- Seen persistence ---
     try:
-        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
         payload = {
             "chat_id": TELEGRAM_CHAT_ID,
             "text": text,
